d04c7d131ac06c5e5bb60443b478ca341dd7b0b2
huangjiandong <huangjiandong@meideng.net>
2021-11-28 15:28:43 +0800

    ADD: 增加网络超时配置

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,13 @@
 export default {
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ["pages/index/index"],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black"
+  },
+  networkTimeout: {
+    request: 1000 * 1,
+    connectSocket: 1000 * 1
   }
-}
+};
